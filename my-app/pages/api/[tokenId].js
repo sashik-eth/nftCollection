f3f1384e8ba753d85981b96a4c693d0520a5705f
MyBook <mybook@MyBooks-MacBook-Pro.local>
2022-05-11 19:46:24 +0300

    update url

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   // get the tokenId from the query params
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
-  const image_url = "https://nft-collection-inky-zeta.vercel.app/cryptodev/";
+  const image_url = "https://nft-collection-inky-zeta.vercel.app/cryptodevs/";
   // The api is sending back metadata for a Crypto Dev
   // To make our collection compatible with Opensea, we need to follow some Metadata standards
   // when sending back the response from the api
