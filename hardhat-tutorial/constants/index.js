56d8aeda96ee8c249e2608f07f8e1a161a4805ba
MyBook <mybook@MyBooks-MacBook-Pro.local>
2022-05-11 19:36:58 +0300

    update contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xEB1D1036EFe1895747c5BB72Ed799F82b5a3a43B";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-inky-zeta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
